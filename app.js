f61e6efa1e0eb4d7f30462177fb2c8e9b166e3f5
Combos0 <jcx429.sc2@gmail.com>
2023-01-12 12:13:34 -0500

    now lets users search for a random gif by key word

--- app.js
+++ app.js
@@ -16,10 +16,25 @@ function gifStart() {
 const elements = {
     img: document.getElementById('content'),
     BTN: document.getElementById('gifBTN'),
+    search: document.getElementById('search'),
 };
 
 gifStart();
 
 const eventHandlers = (() => {
-    elements.BTN.addEventListener('click', gifStart);
+    elements.BTN.addEventListener('click', searchGif);
 })();
+
+function searchGif() {
+    let keyWord = elements.search.value;
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=t2ilWvAtz6EDx36HEayD5xPBM0uHMRoj&q=${keyWord}&limit=1&offset=0&rating=g&lang=en`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        elements.img.src = response.data[0].images.original.url;
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+};
